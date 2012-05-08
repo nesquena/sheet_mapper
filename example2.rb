4d666a5963ab5dbb75b756208d2aa4978b0279b4
Nathan Esquenazi <nesquena@gmail.com>
2012-05-08 01:50:49 -0700

    Adds second example

---
+++ example2.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'active_support/all'
+require 'google_spreadsheet'
+require File.expand_path('lib/sheet_mapper')
+
+class GradeMapper < SheetMapper::Base
+  columns :topic, :grade, :score
+
+  def valid_row?
+    self[:topic].present? && @pos > 3
+  end
+
+  # Convert is_notable to boolean
+  def score
+    self[:score].to_i
+  end
+end
+
+sheet = SheetMapper::Spreadsheet.new(:mapper => GradeMapper, :key => ENV['SHEET_KEY'], :login => ENV['SHEET_LOGIN'], :password => ENV['SHEET_PASS'])
+collection = sheet.find_collection_by_title('data')
+
+name        = collection.cell(1, 2)
+age         = collection.cell(2, 2)
+
+puts "Name: #{name}, Age: #{age}"
+
+bubbles = collection.each do |bubble|
+  p bubble.attributes
+end
+
