bc0fb4f0a1466ea33cf5df3b4730509348b3609d
Nathan Esquenazi <nesquena@gmail.com>
2012-05-15 23:25:08 -0700

    Adds better testing for collection class

--- test/collection_test.rb
+++ test/collection_test.rb
@@ -1,5 +1,85 @@
 require File.expand_path('../test_config.rb', __FILE__)
 
-describe "Collection" do
+class TestMapper
+  attr_reader :index, :record
 
+  def initialize(index, record)
+    @index  = index
+    @record = record
+  end
+
+  def tuple; [name, age]; end
+  def name; record[:name]; end
+  def age;  record[:age]; end
+  def valid_row?; true; end
+end # TestMapper
+
+describe "Collection" do
+  setup do
+    @worksheet   = stub(:rows => [{ :name => "Bob", :age => 21 }, { :name => "Susan", :age => 34 }, { :name => "Joey", :age => 67 }])
+    @spreadsheet = stub(:mapper => TestMapper)
+  end
+
+  context "for worksheet accessor" do
+    setup do
+      @collection = SheetMapper::Collection.new(@spreadsheet, @worksheet)
+    end
+
+    should "return worksheet from accessor" do
+      assert_equal @worksheet, @collection.worksheet
+    end
+  end # worksheet
+
+  context "for each method" do
+    setup do
+      @collection = SheetMapper::Collection.new(@spreadsheet, @worksheet)
+    end
+
+    should "support iterating each mapped row" do
+      rows = []
+      @collection.each { |c| rows << c }
+      assert_equal 3, rows.size
+      assert_equal ["Bob", 21], rows[0].tuple
+      assert_equal ["Susan", 34], rows[1].tuple
+      assert_equal ["Joey", 67], rows[2].tuple
+    end
+  end # each
+
+  context "for rows method" do
+    setup do
+      @collection = SheetMapper::Collection.new(@spreadsheet, @worksheet)
+    end
+
+    should "return raw data hashes" do
+      rows = @collection.rows
+      assert_equal ["Bob", 21], [rows[0][:name], rows[0][:age]]
+      assert_equal ["Susan", 34], [rows[1][:name], rows[1][:age]]
+      assert_equal ["Joey", 67], [rows[2][:name], rows[2][:age]]
+    end
+  end # rows
+
+  context "for cell method" do
+    setup do
+      @worksheet.expects(:[]).with(4,5).returns("foo")
+      @collection = SheetMapper::Collection.new(@spreadsheet, @worksheet)
+    end
+
+    should "return raw data in worksheet" do
+      assert_equal "foo", @collection.cell(4,5)
+    end
+  end # cell
+
+  context "for records method" do
+    setup do
+      @collection = SheetMapper::Collection.new(@spreadsheet, @worksheet)
+    end
+
+    should "support iterating each mapped row" do
+      rows = @collection.records
+      assert_equal 3, rows.size
+      assert_equal ["Bob", 21], rows[0].tuple
+      assert_equal ["Susan", 34], rows[1].tuple
+      assert_equal ["Joey", 67], rows[2].tuple
+    end
+  end # records
 end
