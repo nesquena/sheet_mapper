01285640347f2762263a026852f608d8d2af8c27
Anshul Khandelwal <anshul@anshulkhandelwal.com>
2013-09-23 20:10:23 +0530

    Added ability to find spreadsheet by title and url

--- lib/sheet_mapper/spreadsheet.rb
+++ lib/sheet_mapper/spreadsheet.rb
@@ -6,7 +6,7 @@ module SheetMapper
     def initialize(options={})
       @mapper   = options[:mapper]
       @session = ::GoogleDrive.login(options[:login], options[:password])
-      @spreadsheet = @session.spreadsheet_by_key(options[:key])
+      @spreadsheet = find_spreadsheet(options[:key], options[:url], options[:title])
     end
 
     # Returns the first worksheet with given title
@@ -17,5 +17,14 @@ module SheetMapper
       raise CollectionNotFound, "No worksheet found '#{val}'! Please try again." unless worksheet
       Collection.new(self, worksheet)
     end
+
+    private
+    def find_spreadsheet(key, url, title)
+      raise ArgumentError, "No key, url or title specified for the spreadsheet" unless key || url || title
+      return @session.spreadsheet_by_url(url) if url
+      return @session.spreadsheet_by_title(title) if title
+      @session.spreadsheet_by_key(key)
+    end
+
   end
 end
