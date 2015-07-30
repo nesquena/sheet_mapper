2533d19270491021f7e511b4c7668a61f27ed09e
Derek Lindahl <dlindahl@customink.com>
2015-07-30 13:03:31 -0700

    Prevent mocha from stubbing nonexistent methods
    This is important when upgrading google_drive because the method names
    have changed in 1.0 and keeping this stub in place causes a false
    positive

--- test/test_config.rb
+++ test/test_config.rb
@@ -6,3 +6,4 @@ require File.expand_path('../../lib/sheet_mapper', __FILE__)
 
 Dir[File.expand_path("../test_helpers/*.rb", __FILE__)].each { |f| require f }
 FakeWeb.allow_net_connect = false
+Mocha::Configuration.prevent(:stubbing_non_existent_method)
